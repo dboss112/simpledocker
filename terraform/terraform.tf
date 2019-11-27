ece78bd454e35af020249521c05bb5e2d053b933
Someshkb <swarupd681@gmail.com>
2019-11-28 05:26:19 +0530

    adding terraform

---
+++ terraform/terraform.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+
+		 region = "us-east-2"
+		
+		 access_key = "${var.access_key}"
+		
+		 secret_key = "${var.secrate_key}"
+
+
+}
+
+
+
+resource "aws_instance" "backend" {
+
+		 ami= "ami-0c929bde1796e1484"
+		
+		 instance_type= "t2.micro"
+		
+		 key_name= "${var.key_name}"
+		
+		 vpc_security_group_ids = ["${var.sg-id}"]
+
+}
+
+
+
+resource "null_resource" "remote-exec-1" {
+
+		connection {
+		
+			user= "ubuntu"
+			
+			type= "ssh"
+			
+			private_key = "${file(var.pvt_key)}"
+			
+			host= "${aws_instance.backend.public_ip}"
+		
+		}
+		
+		provisioner "remote-exec" {
+		
+			inline = [
+			
+			"sudo apt-get update",
+			
+			"sudo apt-get install python sshpass -y",
+		
+		]
+		
+		}
+
+}
+
+resource "null_resource" "ansible-main" {
+
+		provisioner "local-exec" {
+		
+			command = <<EOT
+			
+			sleep 100;
+			
+			> jenkins-ci.ini;
+			
+			echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+			
+			export ANSIBLE_HOST_KEY_CHECKING=False;
+			
+			echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+			
+			ansible-playbook --private-key ${var.pvt_key} -i jenkins-ci.ini ./ansible.yml -u ubuntu -v
+			
+			EOT
+		
+		}
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "key_name" {
+
+ default = "AwsKey"
+
+}
+
+variable "access_key" {
+
+ default = "AwsKey"
+
+}
+
+variable "secrate_key" {
+
+ default = "AwsKey"
+
+}
+
+variable "pvt_key" {
+
+ default = "./AwsKey.pem"
+
+}
+
+variable "us-east-zones" {
+
+ default = ["us-east-2"]
+
+}
+
+variable "sg-id" {
+
+ default = "sg-0335d1051255a7431"
+ 
+}
